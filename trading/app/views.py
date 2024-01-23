176ec17b361c5f5123601268e2373a8cb78e740e
Lebarondecharlus <git@lebaron.sh>
2024-01-23 14:29:04 +0100

    Gotify support

--- trading/app/views.py
+++ trading/app/views.py
@@ -4,6 +4,8 @@ import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from gotify import Gotify
+
 from kraken.futures import Trade, User
 from kraken.spot import User as Suser, Trade as Strade
 
@@ -12,20 +14,36 @@ from .models import Order
 ##########################
 # Config
 ##########################
+
+sandbox = os.environ.get('SANDBOX')
 key = os.environ.get('KEY')
 secret = os.environ.get('SECRET')
 spot_key = os.environ.get('SPOT_KEY')
 spot_secret = os.environ.get('SPOT_SECRET') 
+gotify_host = os.environ.get('GOTIFY_HOST')
+gotify_token = os.environ.get('GOTIFY_TOKEN')
+
+gotify = Gotify(
+    base_url = f"{gotify_host}",
+    app_token = f"{gotify_token}"
+)
+
 
 # Create your views here.
 def ping(request):
+    gotify.create_message(
+        "pong",
+        title="ping",
+        priority=0,
+    )
+    
     data = {"ping": "pong"}
     return JsonResponse(data)
 
 @csrf_exempt
 def trade(request):
-    trade = Trade(key=key, secret=secret, sandbox=True)
-    user = User(key=key, secret=secret, sandbox=True)
+    trade = Trade(key=key, secret=secret, sandbox=sandbox)
+    user = User(key=key, secret=secret, sandbox=sandbox)
     strade = Strade(key=spot_key, secret=spot_secret)
     suser = Suser(key=spot_key, secret=spot_secret)
 
@@ -59,6 +77,11 @@ def trade(request):
                 long_order = trade.create_order(**dict(futures_long))
                 #order = Order.objects.create(order_id=long_order['clientOrderId'], order_direction="long", order_amount_open=long_order['origQty'], order_amount=long_order['origQty'])
                 #order.save()
+                gotify.create_message(
+                    "Futures Long",
+                    title=f"Futures open long position",
+                    priority=0,
+                )
                 print(long_order)
                 return JsonResponse(long_order)
 
@@ -66,6 +89,11 @@ def trade(request):
                 short_order = trade.create_order(**dict(futures_short))
                 #order = Order.objects.create(order_id=long_order['clientOrderId'], order_direction="long", order_amount_open=long_order['origQty'], order_amount=long_order['origQty'])
                 #order.save()
+                gotify.create_message(
+                    "Futures Short",
+                    title=f"Futures open short position",
+                    priority=0,
+                )
                 print(short_order)
                 return JsonResponse(short_order)
 
@@ -87,6 +115,12 @@ def trade(request):
                                     side=side,
                                     size=size
                                 )
+                    gotify.create_message(
+                        "Close Futures",
+                        title=f"Futures close position {side}",
+                        priority=0,
+                    )
+
                     print(close_position)
                     return JsonResponse(close_position)
                 else :
@@ -105,6 +139,12 @@ def trade(request):
                 long_order = strade.create_order(ordertype='market',pair='BTCUSD', side='buy',volume=size, leverage='3')
                 #order = Order.objects.create(order_id=long_order['clientOrderId'], order_direction="long", order_amount_open=long_order['origQty'], order_amount=long_order['origQty'])
                 #order.save()
+                gotify.create_message(
+                    "Spot Long",
+                    title=f"Spot enter long position",
+                    priority=0,
+                )
+
                 print(long_order)
                 return JsonResponse(long_order)
 
@@ -112,6 +152,12 @@ def trade(request):
                 short_order = strade.create_order(ordertype='market',pair='BTCUSD', side='sell',volume=size, leverage='3')
                 #order = Order.objects.create(order_id=long_order['clientOrderId'], order_direction="long", order_amount_open=long_order['origQty'], order_amount=long_order['origQty'])
                 #order.save()
+                gotify.create_message(
+                    "Spot Short",
+                    title=f"Spot enter short position",
+                    priority=0,
+                )
+
                 print(short_order)
                 return JsonResponse(short_order)
             
@@ -127,6 +173,12 @@ def trade(request):
                         return 
                     
                     close_position = strade.create_order(ordertype='market',pair='BTCUSD', side=side,volume=size, leverage='3')
+                    gotify.create_message(
+                        "Close Spot",
+                        title=f"Futures close position {side}",
+                        priority=0,
+                    )
+
                     print(close_position)
                     return JsonResponse(close_position)
                 else :
@@ -142,7 +194,7 @@ def trade(request):
 
 @csrf_exempt
 def account(request):
-    user = User(key=key, secret=secret, sandbox=True)
+    user = User(key=key, secret=secret, sandbox=sandbox)
            
     # GET ACCOUNT INFO
     res = user.get_wallets()
